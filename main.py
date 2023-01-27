5fd6da36978d61a816739c2af348a87cd84b3620
Rlaude06 <43902216+Rlaude06@users.noreply.github.com>
2023-01-27 19:39:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from tkinter import *
 from PIL import Image, ImageTk
 import os
-# module internes
+# modules internes
 import radio, image_opener, notes, decode, faille, default
 from positions import check_click
 
@@ -31,23 +31,6 @@ intro_strs = [open("data/txt0", encoding="utf-8").read(),open("data/txt1", encod
 # Identifiant de la page actuelle
 current_page=0
 
-# Changer le fond et le texte affiché à l'écran
-def update_image():
-    global current_page
-    current_page+=1
-    if current_page==3: # Quand arrivé sur le bureau détruire le bouton suivant ainsi que le Label qui contient le texte affiché à l'écran
-        can.itemconfig(image_container,image=backgrounds["default"])  
-        info_text.destroy()
-        next_button.destroy()
-    else:
-        info_text.config(text=intro_strs[current_page])
-
-
-
-# Canvas qui contient tous les widgets de l'écran
-can = Canvas(win, highlightthickness=0)
-can.pack(fill='both', expand=1)
-
 # Fenêtre correspondant au click sur l'imprimante
 def imprimante():
     win2 = Toplevel()
@@ -55,17 +38,20 @@ def imprimante():
     Label(win2, text="Il semblerai qu'un fichier contenu dans "+os.path.abspath("files/")+"\nsoit protégé par un mot de passe rendant l'impression impossible...").pack()
     win2.mainloop()
 
+
 # Fonction gérant le click de la souris sur l'écran
 def handle_click(event):
     if current_page!=3: # Quitter la fonction si le joueur n'est pas sur le bureau
         return
 		
     x, y = event.x, event.y # Coordonnées du click
-    new_win_id = check_click(x,y,w,h) # Voir positions.py
+    new_win_id = check_click(x,y,w,h) # Voir positions.py > check_click()
 
-    if not new_win_id:
+
+    if not new_win_id: # Si le click ne correspond à aucune zone quitter
         return
     
+    # Ouvrir les fenêtres correspond à leur zone
     if new_win_id=="radio":
         radio.run_radio()
 
@@ -82,24 +68,41 @@ def handle_click(event):
         imprimante()
 
     elif new_win_id in ("poeme", "phone", "ordi", "chocolat"):
-        image_opener.open_image(new_win_id)
+        image_opener.open_image(new_win_id) # voir image_opener.py
+
+# Changer le fond et le texte affiché à l'écran
+def update_image():
+    global current_page
+    current_page+=1
+    if current_page==3: # Quand arrivé sur le bureau détruire le bouton suivant ainsi que le Label qui contient le texte affiché à l'écran
+        can.itemconfig(image_container,image=backgrounds["default"])  
+        info_text.destroy()
+        next_button.destroy()
+    else:
+        info_text.config(text=intro_strs[current_page])
 
 
+# Canvas qui contient tous les widgets de l'écran
+can = Canvas(win, highlightthickness=0)
+can.pack(fill='both', expand=1)
 
+# Image contenant le fond
 image_container = can.create_image(0,0, anchor="nw",image=backgrounds["black"])
 
+# Définir la fonction associé au click gauche de la souris
 can.bind("<Button-1>", handle_click)
 
-
+# Bouton  de fermeture du jeu 
 close_button = Button(can, text="Quitter le jeu", bg="black", fg="red", command=win.destroy, highlightthickness=0, font='Helvetica 18 bold')
 close_button.pack(side=BOTTOM)
 
-
-
+# Bouton pour passer à l'étape suivante de l'introduction
 next_button = Button(can, text="Suivant", bg="black", fg="white", command=update_image)
 next_button.pack(side=BOTTOM)
 
-
+# Texte d'introduction
 info_text = Label(can, text = intro_strs[0], fg="white", bg="black", justify=LEFT, font=("Helvetica",30))
 info_text.pack(side=BOTTOM)
+
+#Lancer le jeu
 win.mainloop()
