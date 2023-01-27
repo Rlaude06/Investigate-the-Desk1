87ad6c4ce9cb25d615806d65e9d5ea668a7fb215
Rlaude06 <43902216+Rlaude06@users.noreply.github.com>
2023-01-27 22:28:55 +0100

    Update image_opener.py

--- image_opener.py
+++ image_opener.py
@@ -2,15 +2,25 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 def open_image(name):
+
+	# Créer une fenêtre
     win = Toplevel()
     win.title(name)
+
+	# Ouvre l'image
     img = PhotoImage(file="img/%s.png" % name)
 
+	# Récupérer la résolution
     w, h = img.width(), img.height()
+
+	# Afficher image
     img_lab = Label(win)
     img_lab.config(image=img)
     img_lab.pack()
+
+# Définir les dimensions de la fenêtre avec la résolution de l'image
     win.minsize(w,h)
     win.maxsize(w,h)
-    win.mainloop()
 
+# Lancer la fenêtre
+    win.mainloop()
